fc90198e403df6d7db93e89b7c7b5c76e77ee40e
RyanWelly <ryanwinter5142@gmail.com>
2024-08-05 15:42:24 +1200

    remove old cruft

--- src/macros.rs
+++ src/macros.rs
@@ -5,276 +5,7 @@
 // Cdr of a list returns the tail
 // Cons of x and y is the list (x y)
 
-// rewrite!(QUOTE x) => x
-// rewrite!()
-
-macro_rules! eval {
-    (QUOTE $rest:tt) => {
-        $rest
-    };
-    (ATOM ) => {}; //For atom, just check if input is an ident or not.
-    (CAR $rest:tt) => {};
-    (CONS $x:tt $y:tt) => {
-        ($x, $y)
-    };
-}
-
-macro_rules! eval_callback {
-    (QUOTE $rest:tt) => {
-        $rest
-    };
-    (ATOM ) => {}; //For atom, just check if input is an ident or not.
-    (CAR $rest:tt) => {};
-    (CONS $x:tt $y:tt) => {
-        ($x, $y)
-    };
-}
-macro_rules! callback {
-    ($callback1:ident $($args:tt)*) => {$callback1!($($args)*)};
-}
-macro_rules! callback2 {
-    ($callback1:ident $callback2:ident $($args:tt)*) => {};
-}
-
-macro_rules! stringify_test {
-    ($($args:tt)*) => {
-        stringify!($($args)*)
-    };
-}
-
-const test: ((&str, &str), &str) = eval!(CONS ("hello", "hel") "hell");
-
-const tost: &str = callback!(stringify(eval(QUOTE)));
-const blah: &str = stringify_test!(hello hi "hi");
-#[test]
-fn quick_test() {
-    println!("{blah}");
-    //to avoid dealing with stringify! being eager, just use trace_macros nightly feature
-}
-
-//probably need to interleave this with the actual computation
-//ie so that
-//eval!(eq a b)
-//expands to something like
-
-//macro_rules! __inner_eq {..}
-//eval!(rest of stuff and uses __inner_eq)
-//NOTE: rust macros are fine with shadowing, and will use the most recently defined one; abuse this to all hell.
-macro_rules! eq {
-    ($lhs:tt $rhs:tt) => {
-        macro_rules! __eq {
-                                            ($lhs $lhs) => "true";
-                                            ($lhs $rhs) => "false";
-                                        }
-    __eq($lhs $rhs)
-    };
-
-}
-
-macro_rules! car {
-    () => {};
-}
-
-//minimal viable example - no recursive eval (args essentially all quoted by default)
-
-macro_rules! min_eval {
-    (QUOTE $rest:tt) => {
-        $rest
-    };
-
-    (CAR ($head:tt $rest:tt)) => {
-        $head
-    };
-
-    //might need a (CDR ($head:tt $($rest)+:tt)) or whatever
-    (CDR ($head:tt $rest:tt)) => {
-        $rest
-    };
-
-    (ATOM $atom:ident) => {
-        TRUE
-    };
-    (ATOM $t:tt) => {
-        NIL
-    };
-
-    (CONS $x:tt $y:tt) => {
-        ($x $y)
-    };
-}
-
-//top level
-
-macro_rules! rec_eval {
-    (QUOTE $rest:tt) => {
-        $rest
-    };
-
-    //So, what do we actually want here?
-    //ideally, we want something like:
-    // (ATOM $t:tt) => {
-    //     rec_eval!(@ ATOM rec_eval!($t));
-    // };
-    // but doing this directly is not possible, since rust macros aren't evaulated eagerly (unless you're proc macro or doing Yandros shit).
-    // so instead, we call inner
-    (ATOM $t:tt) => {
-        macro_rules! inner_atom {
-            ()
-        }
-    };
-
-    (@ ATOM $atom:ident) => {
-        TRUE
-    };
-    (@ ATOM $t:tt) => {
-        NIL
-    };
-}
-
-//I want
-
-macro_rules! with_evaled {
-    () => {};
-}
-
-//words from the yandros above:
-
-// This is the question of eager expansion.
-// Basically, it depends on whether you're the author of a! or b!.
-
-// If you're the author of b!, then there is a trick to have the invocation of your macro be the outer one (so that it happens first), and then give whatever it had to give to a!:
-// macro_rules! with_b {( /* args of b ,*/ $($rules:tt)* ) => (
-//     macro_rules! __emit__ { $($rules)* }
-//     __emit__! {
-//         /* your macro "output" */
-//     }
-// )}
-
-// so that you can then write:
-// with_b! {( /* args of b ,*/ ( $($output_of_b:tt)* ) => (
-//     a! { $($output_of_b)* }
-// )}
-
-//vaguely working example of what I want
-
-// #![feature(trace_macros)]
-
-// macro_rules! rec_eval {
-//     (QUOTE $rest:tt) => {
-//         $rest
-//     };
-
-//     (ATOM $t:tt) => {
-//         callback!(rec_eval(@ ATOM rec_eval!$t:tt))
-//     };
-
-//     (@ ATOM $atom:ident) => {
-//         TRUE
-//     };
-//     (@ ATOM $t:tt) => {
-//         NIL
-//     };
-// }
-
-// macro_rules! callback {
-//     ($callback:ident( $($args:tt)* )) => {
-//         $callback!( $($args)* )
-//     };
-// }
-
-// fn main() {
-//     trace_macros!(true);
-//     rec_eval!(ATOM (QUOTE x))
-// }
-
-// NEW PLAN
-// STACK MACHINES MY BELOVED
-
-// implementing a simple lisp with QUOTE, ATOM, EQ.
-// Quote: pushes entire next s expression onto the stack, without breaking it up.
-// Atom: pop top from data stack, if it is an atom put TRUE on top of the data stack, otherwise NIL.
-// EQ: pop two top from data stack, if equal push TRUE, else push NIL. Generate an interior macro to test equality.
-
-// format/decoding:
-// stack_lisp!(  (ATOM X) [ ]  [ ])
-//                   1     2    3
-// 1. S-expression
-// 2. Operand/control stack
-// 3. Data stack
-
-// For primitives that have a single arg, it is relatively easy; eg `(ATOM X)` => `X` [Atom] [] => `` [ATOM] [X] => `` [] [T]
-// once we have fully parsed the lisp into a stack based form, we can then interpret the result:
-// - pop the top operator, and then pop however many operands the operator needs from the data stack, compute result, put on top of data stack.
-
-// unresolved questions:
-// - handling recursion? Lambda? label?
-// - handling proper lists vs dotted lists? investigate handling Cons
-// - maybe use a different token to deliminate the stacks than '[' and ']'
-// - can this scheme deal with things like cond and weird orders of evaluation?
-// - can I split up 'lifetime into ' and lifetime? That way I could use a "lisp macro" to implement that 'a == (quote x). Might have to use a different character.
-
-// for eq, instead of expanding a macro above and using the macro inside, I can instead branch the whole thing inside a macro.
-// maybe use https://veykril.github.io/tlborm/decl-macros/patterns/tt-bundling.html instead of having $($token_tree:tt)* everywhere
-
 //can use lambda for most things except for quote https://news.ycombinator.com/item?id=8715655
-
-//first iteration: only has instructions which accept a single argument
-
-macro_rules! stack_lisp_v1 {
-
-
-    // use another macro as top level ie
-    // macro_rules! lisp {($($sexprs:tt)* ) => stack_lisp!($($sexprs)* [] [])}
-    // maybe split up parsing, executing into different macros?
-
-    ( (ATOM $toks:tt) [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!( $toks [ATOM $($operands)*] [$($data)*])};
-    ( (QUOTE $toks:tt) [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!( [$($operands)*] [$toks $($data)*])}; //just puts the rest of the args onto the data stack (is this right???)
-    ( (CAR $toks:tt) [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!( $toks [CAR $($operands)*] [$($data)*])};
-    ( (CDR $toks:tt) [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!( $toks [CDR $($operands)*] [$($data)*])};
-
-    ( $val:ident [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!( [ $($operands)*] [$val $($data)*])}; //need to figure out how to do this in the presence of variable bindings. Maybe don't need two stacks?
-    ( $toks:tt [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [$toks $($data)*])};
-    ( [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [$($data)*])};
-
-
-
-    // ATOM operation
-    (@eval [ATOM $($operands:tt)*] [ $atom:ident $($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [ TRUE $($data)*])};
-    (@eval [ATOM $($operands:tt)*] [ $list:tt $($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [ NIL $($data)*])};
-
-    // CAR operation
-    (@eval [CAR $($operands:tt)*] [ ($list:tt) $($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [ $list $($data)*])};              // (car (list)) = list
-    (@eval [CAR $($operands:tt)*] [ ($list:tt $($rest:tt)*) $($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [ $list $($data)*])}; // (car(list ....)) = list
-
-    // CDR operation
-    (@eval [CDR $($operands:tt)*] [ ($list:tt) $($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [ NIL $($data)*])};                // (cdr (list)) = nil
-    (@eval [CDR $($operands:tt)*] [ ($list:tt $($rest:tt)*) $($data:tt)*]) => {stack_lisp_v1!(@eval [$($operands)*] [ $($operand)* $($data)*])}; // (cdr (list ....)) = ....
-    (@eval [CDR $($operands:tt)*] [ $invalid:ident $($data:tt)*]) => {"error: cdr cannot be called on atoms"};
-
-
-    // CONS operation (not implemented yet)
-    (@eval [CONS $($operands:tt)*] [ $a:tt $b:tt $($data:tt)*]) => {stack_lisp!(@eval [$($operands)*] [ ($a $b) $($data)*])};              // (cons x y) = (x y)
-
-
-    //finish and print final evaluation
-    (@eval [] [$val:tt]) => {stringify!($val)};
-
-
-}
-
-//TODO: change the stacks to be something other than open and closed brackets, otherwise $($rest:tt)* will be too greedy.
-// might need something to denote whether we're consuming arguments, to ensure our stack_lisp macro is only consuming a single lisp expression.
-macro_rules! stack_lisp_v2 {
-    ( (ATOM $toks:tt) $($rest:tt)* @ $($operands:tt)* @ [$($data:tt)*]) => {stack_lisp_v2!( $toks $($rest)* @ATOM $($operands)*@ [$($data)*])};
-    ( (QUOTE $toks:tt) $($rest:tt)* @$($operands:tt)*@ [$($data:tt)*]) => {stack_lisp_v1!( $($rest:tt)* @$($operands)*@ [$toks $($data)*])};
-
-    ( $val:ident $($rest:tt)* [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v2!($($rest:tt)* [ $($operands)*] [$val $($data)*])}; //need to figure out how to do this in the presence of variable bindings. Maybe don't need two stacks?
-    ( $toks:tt [$($operands:tt)*] [$($data:tt)*]) => {stack_lisp_v2!(@eval [$($operands)*] [$toks $($data)*])};
-}
-
-macro_rules! lisp {
-    ($($toks:tt)+) => {internal_lisp!(stack: [] env: []  control: ($($toks)+)  dump: [] )};
-}
 
 // also ahhhhhh calling a function with the wrong number of elements is undefined behaviour in our scheme
 // ((IF X CONS CAR) (QUOTE (x y))) is immediate ub if X is truthy. We do check the number of args being given straight to a primitive though
@@ -330,9 +61,16 @@ macro_rules! internal_lisp {
     (stack: [$($stack_entries:tt)*] env: $env:tt control: [DISPLAY $($rest:tt)*] dump: $dump:tt) => {
         internal_lisp!(stack: [@DISPLAY $($stack_entries)*] env: $env control: [$($rest)*] dump: $dump)
     };
+    (stack: [$($stack_entries:tt)*] env: $env:tt control: [ATOM $($rest:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: [@ATOM $($stack_entries)*] env: $env control: [$($rest)*] dump: $dump)
+    };
 
 
     // Evaluate primitives - top of the stack
+    // TODO:
+    // - EQ
+    // - CONS
+    // - ATOM
 
 
     // CAR
@@ -354,10 +92,6 @@ macro_rules! internal_lisp {
         internal_lisp!(stack: [$car $($stacks)*] env: $env control: [$($controls)*] dump: $dump)}
     };
 
-    // TODO:
-    // - EQ
-    // - CONS
-    // -
 
 
     // pop closure from the top of the stack, and the closure's variable is mapped to the value.
@@ -411,6 +145,10 @@ macro_rules! error {
 
 }
 
+macro_rules! lisp {
+    ($($toks:tt)*) => {internal_lisp!(stack: [] env: []  control: [($($toks)*)]  dump: [] )};
+}
+
 // we store an environment as an association list ([var_name_1: val, var_name_2: other_val])
 // to retrieve a value, we create an inner macro with an arm for each key
 macro_rules! env_test {
@@ -425,66 +163,8 @@ macro_rules! env_test {
     }
 }
 
-// l => left is hole
-// r => right is hole
-// if a binding of variable is not found in env, just return itself.
-// lisp style syntax, (\x. x) and ((\y. x)(\x. x)) is valid ony with the outside parens.
-// (LAMBDA (X) T)
-macro_rules! cek {
-
-    (@ $x:ident, {$($key:ident : $val:tt)*}, $k:tt) => { // map to env
-        {
-        macro_rules! consult_env {
-            $(
-                ($key) => {cek!(@ $k, $val)};
-            )*
-            ($x) => {cek!(@ $k,$x)}; //if not in enviroment, we just evaluate it to itself so that ie (\x.y)x -> y.
-        }
-        consult_env!($x)
-        }
-    };
-    (@ (λ $x:ident. $arg:tt), $env:tt, $k:tt) => { //handles lambdas
-        cek!(@ $k, [$x, $arg, $env])
-    };
-    (@ ($t1:tt $t2:tt), $env:tt, $k:tt) => { //handles application case
-        cek!(@ $t1, $env, (arg: $t2, $env, $k))
-    };
-
-    //operate on values
-    (@ (arg: $t:tt, $env:tt, $k:tt), $v:tt) => {
-        cek!(@ $t, $env, (fun: $v, $k))
-    };
-    (@ (fun: [$x:ident, $t:tt, {$($key:ident : $val:tt)*}], $k:tt), $v:tt) => {
-        cek!(@ $t, {$x : $v $($key:$val)*}, $k)
-    };
-    (@ stop, $v:tt) => {stringify!($v)}
-}
-
-macro_rules! cek_call {
-    ($($terms:tt)*) => {
-        cek!( @ $($terms)* , {}, stop)
-    };
-}
-
-#[test]
-fn cek_test() {
-    let _single_var = cek_call!(x);
-    let _single_lamba = dbg!(cek_call!(((λ x. y) x )  ));
-    let _single_lamba = dbg!(cek_call!(((λ y.  (y y)) (λ x. x) )  ));
-    let _nested = dbg!(cek_call!((λ x. ((λ y. y)x)) )); //evaluates to itself, since no arguments given
-    let _nested = dbg!(cek_call!(((λ f. (λ x. (f x))) (λ y. (λ x. y))))); //evaluates to itself, since no arguments given
-    let _nested = dbg!(cek_call!(((λ x.x)(λ y.(λ y.(λ x.x)))))); //evaluates to itself, since no arguments given
-                                                                 // let nested = cek_call!(((λ x. (x x)) (λ x. (x x)))); //loops forever
-}
-
 #[test]
 fn stack_lisp_test() {
-    let hello = stack_lisp_v1!( (ATOM (QUOTE hello)) [] []);
-    println!("{hello}");
-    let hello = stack_lisp_v1!( (CDR (QUOTE (hello))) [] []);
-    println!("{hello}");
-    let hello = stack_lisp_v1!( (QUOTE (QUOTE (hello))) [] []);
-    println!("{hello}");
     internal_lisp!(stack: [random nonsense] env: [a: (A B)] control: [a] dump: []);
     dbg!(internal_lisp!(stack: [] env: [] control: [(CDR (QUOTE (X Y)))] dump: []));
     let hello = internal_lisp!(stack: [] env: [a: (A B)] control: [ (DISPLAY(CDR a))] dump: []);
@@ -492,13 +172,17 @@ fn stack_lisp_test() {
     //     internal_lisp!(stack: [] env: [] control: [((LAMBDA (x) (CDR X)) (QUOTE (A B)))] dump: [])
     // );
     let hello =
-        dbg!(internal_lisp!(stack: [] env: [] control: [((LAMBDA (x) x)(QUOTE x))] dump: []));
+        dbg!(internal_lisp!(stack: [] env: [] control: [((LAMBDA (x) x)(QUOTE y))] dump: []));
     let hello = dbg!(
         internal_lisp!(stack: [] env: [] control: [((LAMBDA (y) (CDR y))(QUOTE (x)))] dump: [])
     );
     let invalid = dbg!(
         internal_lisp!(stack: [] env: [] control: [((LAMBDA (y) (CDR x))(QUOTE (x)))] dump: [])
     );
+    let top_level_test = lisp!(QUOTE X);
+    lisp!(LAMBDA (x) (CDR x));
+    lisp!((LAMBDA (x) (x (QUOTE (A B)))) CAR); //this leads to an error, since CAR is not found in environment. TODO: rejig so that the primitives are stored in env too
+    dbg!(internal_lisp!(stack: [] env: [CAR: @CAR] control: [(CAR (QUOTE (a)))] dump: []));
 }
 // Desription of my lisp:
 

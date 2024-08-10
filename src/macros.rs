b50a2d6232816d2c3e189e7b380766e3549b28db
RyanWelly <ryanwinter5142@gmail.com>
2024-08-10 23:26:31 +1200

    Add define, cond, list

--- src/macros.rs
+++ src/macros.rs
@@ -32,6 +32,18 @@ macro_rules! internal_lisp {
     (stack: [$($stacks:tt)*] env: $env:tt control: [(LAMBDA ($name:ident) $T:tt) $($controls:tt)*] dump: $dump:tt) => {
         internal_lisp!(stack: [[$name, $T, $env] $($stacks)*] env: $env control: [$($controls)*] dump: $dump)
     };
+    //TODO: make multiple args work for lambdas
+    // probably just automatically curry, ie make (lambda (x y z) T) => (lambda (x) (lambda (y) (lambda (z) T)))
+    // actually, that kinda sucks
+
+    // IDEA: if you have eg
+    // macro_rules! test {
+    //     ($($repeat:ident)* : $($other_repeat) ) => { $($repeat, $other_repeat)*}
+    // }
+    // it will only work if there are the same number of repeats in both matchings. Use this somehow to implement?
+    // probably have to construct a list of all the args to be fed to the closure
+
+
 
 
     // IF special form
@@ -39,13 +51,74 @@ macro_rules! internal_lisp {
     // Although, I could totally just implement `cond` at this point;
     // (COND (p1 e1) .... (pn en)) => p1 :: (IFS e1 (COND (p2 e2) ... (pn en)) )
     // (COND (p1 e)) => p1 :: (IFS e nil)
+    // TODO - working on cond
+
+
+
+    // TODO - test this works
+    (stack: $stack:tt env: $env:tt control: [(COND ($p:tt $e:tt) $(($pn:tt $en:tt))+ )  $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: $stack env: $env control: [$p {__IFS $e (COND $(($pn $en))+  ) } $($control)*] dump: $dump)
+    }; //TODO - maybe have {IFS ...} instead of (IFS ...)
+    (stack: $stack:tt env: $env:tt control: [(COND ($p:tt $e:tt)) $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: $stack env: $env control: [$p {__IFS $e NIL } $($control)*] dump: $dump)
+    }; // If we reach the end of the COND without a predicate matching, we return nil
+
+    (stack: [() $($stack:tt)*] env: $env:tt control: [{__IFS $p:tt $e:tt } $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: [$($stack)*] env: $env control: [ $e $($control)*] dump: $dump)
+    }; // NIL/false case for the __IFS
+
+    (stack: [$val:tt $($stack:tt)*] env: $env:tt control: [{__IFS $p:tt $e:tt } $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: [$($stack)*] env: $env control: [ $p $($control)*] dump: $dump)
+    }; // truthy case for the __IFS - everything but nil/empty list is true
+
+
+
+
+
+
+
+
 
 
     // Define special form
     // semantics: can use at top level or any level at all, (define name exp) just evalutes exp in the current env, then binds it to `name` in the current env.
+    // Returns nil.
     // (DEFINE name expr) => expr :: {__DEFINE name}
+    // TODO - test
 
 
+
+    (stack: $stack:tt env: $env:tt control: [ (DEFINE $name:ident $exp:tt) $($rest:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: $stack env: $env control: [$exp {__DEFINE:name} $($rest)*] dump: $dump)
+    };
+
+    (stack: [$value:tt $($stack:tt)*] env: [$($key:ident : $val:tt)*] control: [{__DEFINE:$name:ident} $($rest:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: [() $($stack)*] env: [$name: $value $($key : $val)*] control: [$($rest)*] dump: $dump )
+    };
+
+    //PROGN special form
+    // TODO
+    // basically a hack to be able to write multiple lisp expressions
+    // (PROGN e1 e2 .. eN) evaluates e1, e2, etc then returns eN's value
+
+
+    (stack: $stack:tt env: $env:tt control: [(PROGN $($forms:tt)*) $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: $stack env: $env control: [])
+    };
+    //how to implement? maybe transform stack: [(PROGN $($exprs:tt)*)] => stack [(list $($exprs)*), __LAST]
+    // which means TODO: make list primitive
+
+
+
+    // List - not a special form, but needs special handling
+
+    (stack: $stack:tt env: $env:tt control: [(LIST $head:tt $($args:tt)*)  $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: $stack env: $env control: [(CONS $head (LIST $($args)*)) $($control)*] dump: $dump)
+    };
+
+    (stack: $stack:tt env: $env:tt control: [(LIST)  $($control:tt)*] dump: $dump:tt) => {
+        internal_lisp!(stack: $stack env: $env control: [NIL $($control)*] dump: $dump)
+    };
 
 
     // (t1 t2) => t2::t1::ap
@@ -61,8 +134,7 @@ macro_rules! internal_lisp {
 
     // Evaluate primitives - top of the stack
     // TODO:
-    // - EQ
-    // - ATOM
+    // - LIST
 
     // TODO: maybe evalute () to NIL? Decide properly how I handle NIL vs () vs '()
 
@@ -114,8 +186,6 @@ macro_rules! internal_lisp {
     (stack: [__CONS ($($list:tt)*) $val:tt $($stack_entries:tt)*] env: $env:tt control: [ap $($rest:tt)*] dump: $dump:tt) => {
         internal_lisp!(stack: [ ($val $($list)*)  $($stack_entries)*] env: $env control: [$($rest)*] dump: $dump)
     };
-    //TODO: decide on my representation of NIL; will I use the empty list here and just a binding of NIL: () to the env?
-    // I've decided to use the internal representation of the empty list for nil, so that NIL evalutes to (), but that () is also not self evaluating.
 
 
     // pop closure from the top of the stack, and the closure's variable is mapped to the value.
@@ -124,7 +194,7 @@ macro_rules! internal_lisp {
         internal_lisp!(stack: [] env: [$var:$v $($key:value)*] control: [$T] dump: [([$($stacks)*], $env, [$($controls)*]) $($dump)*])
     };
 
-    // done processing the current closure, pop stack/env/control of the dump and continue evaluating
+    // done processing the current closure, pop stack/env/control off the dump and continue evaluating
 
     (stack:[$val:tt $($stack_entries:tt)*] env: $env:tt control: [] dump: [ ([$($prev_stack:tt)*], $prev_env:tt, $prev_control:tt) $($rest_of_dump:tt)* ]  ) => {
         internal_lisp!(stack: [$val $($prev_stack)*] env: $prev_env control: $prev_control dump: [$($rest_of_dump)*])
@@ -167,7 +237,8 @@ macro_rules! lisp { //call internal_lisp! with the default env
         DISPLAY:__DISPLAY
         EQ: __EQ
         CONS: __CONS
-        NIL: ()]
+        NIL: ()
+        TRUE: TRUE]
         control: [($($toks)*)]
         dump: [] )};
 }
@@ -248,8 +319,45 @@ mod tests {
     }
 
     #[test]
+    fn list() {
+        let test = dbg!(lisp!(LIST (QUOTE A) NIL));
+        assert_eq!(lisp!(LIST (QUOTE A) (QUOTE B)), stringify!((A B)));
+    }
+
+    #[test]
     fn conditionals() {
-        // assert_eq!(lisp!(COND( NIL TRUE) (TRUE NIL)), "NIL");
+        // assert_eq!(
+        //     lisp!(COND(
+        //         (EQ (QUOTE A ) (QUOTE B))   (QUOTE B)
+        //         (EQ (QUOTE A ) (QUOTE A))   (QUOTE A)
+        //     )
+
+        //     ),
+        //     "B"
+        // );
+        assert_eq!(
+            lisp!(COND
+                ((EQ (QUOTE A ) (QUOTE B))  (QUOTE B)) 
+                ((EQ (QUOTE A ) (QUOTE A))  (QUOTE A))),
+            "A"
+        );
+        assert_eq!(
+            lisp!(COND(
+                (EQ (QUOTE A ) (QUOTE A))   (QUOTE B))),
+            "B"
+        );
+        assert_eq!(
+            lisp!(COND ((EQ (QUOTE A) (QUOTE B)) (QUOTE FIRST))
+                                ((ATOM (QUOTE A)) TRUE)),
+            "TRUE"
+        );
+
+        // let test = lisp!(COND(EQ)); //incorrect forms will usually just fail with some kind of error.
+    }
+
+    #[test]
+    fn define_tests() {
+        assert_eq!(lisp!(DEFINE A NIL), stringify!(()));
     }
     #[test]
     fn non_terminating() {
@@ -263,7 +371,10 @@ mod tests {
 
 // a simple LISP with lexical scoping, implemented fully in the Rust macro system.
 // Avaliable primitives: atom, cons, eq, cons, car, cdr, lambda with a single argument, display.
-// NIL is a shorthand for the empty list, and the empty list also represents falsity. TRUE and every other value is truthy
+// NIL is a shorthand for the empty list, and the empty list also represents falsity. TRUE and every other value is truthy.
+// The define form is a bit weird in my lisp; (define name expr) evaluates expr and then binds it to name in the current env, so there's no restriction to
+// only using it at the top level like there is in Scheme.
+
 // hopefully soon: lambda with arbitary arguments, eval primitive, define primitive.
 // maybe add macros?
 // for proof of concept, write a meta circular interpreter (ie just steal Graham's).
